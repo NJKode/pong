6aece0be77edbdc8f31bdb8810a9e4753a1b1271
unknown <nathan.kennedy.1997@gmail.com>
2025-08-29 18:46:20 +1200

    Opponent can now lose

--- opponent.gd
+++ opponent.gd
@@ -1,13 +1,17 @@
 extends Area2D
 
+@export var speed = 150
+
 var ball: Area2D
 
 func _ready() -> void:
 	ball = get_parent().get_node("Ball")
 
-func _track_ball() -> void:
-	position.y = ball.position.y
+func _track_ball(delta: float) -> void:
+	var y_delta = ball.position.y - position.y
+	var direction = y_delta / abs(y_delta)
 
+	position.y += direction * speed * delta
 
-func _process(_delta: float) -> void:
-	_track_ball()
+func _process(delta: float) -> void:
+	_track_ball(delta)

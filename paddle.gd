267d2ed671fbd0e9dce0d7992decaeb8c35700d3
unknown <nathan.kennedy.1997@gmail.com>
2025-08-31 17:00:36 +1200

    Convert to tabs

--- paddle.gd
+++ paddle.gd
@@ -5,11 +5,11 @@ var vertical_velocity: float
 var screen_size: Vector2
 
 func _ready():
-    vertical_velocity = 0
-    screen_size = get_viewport_rect().size
+	vertical_velocity = 0
+	screen_size = get_viewport_rect().size
 
 
 func _move(delta: float):
-    var new_y_position = position.y + (vertical_velocity * delta)
-    new_y_position = clampf(new_y_position, 0, screen_size.y)
-    position.y = new_y_position
+	var new_y_position = position.y + (vertical_velocity * delta)
+	new_y_position = clampf(new_y_position, 0, screen_size.y)
+	position.y = new_y_position
--- player.gd
+++ player.gd
@@ -3,16 +3,16 @@ extends "res://paddle.gd"
 @export var speed = 400
 
 func _handle_movement(delta: float) -> void:
-    var direction = 0
-    if Input.is_action_pressed("move_up"):
-       direction = -1
-    elif Input.is_action_pressed("move_down"):
-        direction = 1
+	var direction = 0
+	if Input.is_action_pressed("move_up"):
+		direction = -1
+	elif Input.is_action_pressed("move_down"):
+		direction = 1
 
-    vertical_velocity = direction * speed
+	vertical_velocity = direction * speed
 
-    _move(delta)
-    
+	_move(delta)
+	
 
 func _process(delta: float) -> void:
-    _handle_movement(delta)
+	_handle_movement(delta)
